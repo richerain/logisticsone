a926e1397d0b7d2ceb6aa90c71252efe64951a23
ricmatias <altamantericjason@gmail.com>
2025-10-07 10:16:20 +0800

    asd

--- logs1/resources/views/auth/login.blade.php
+++ logs1/resources/views/auth/login.blade.php
@@ -60,6 +60,9 @@
       align-items: center;
     }
     .company-logo img {
+      width: 400px;
+      height: auto;
+      opacity: 1;
     }
     .company-text h1 {
       font-size: 3rem;
@@ -168,7 +171,7 @@
     <div class="section-1">
       <div class="company-content">
         <div class="company-logo mt-2rem">
-          <img class="w-400 h-auto opacity-1" src="{{ asset('images/micrologo.png') }}" alt="Microfinancial Logo">
+          <img src="{{ asset('images/micrologo.png') }}" alt="Microfinancial Logo">
         </div>
         <div class="company-text uppercase relative z-index-2 text-gray-800">
           <h1 class="font-bold text-gold-500">Microfinance I</h1>
