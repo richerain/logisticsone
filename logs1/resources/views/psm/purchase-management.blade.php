5bc5e1fa9626b7d6ccd7a51f193937e6fe6f8fb3
richerain <altamantericjason@gmail.com>
2026-02-12 05:16:26 +0800

    dadad

--- logs1/resources/views/psm/purchase-management.blade.php
+++ logs1/resources/views/psm/purchase-management.blade.php
@@ -497,6 +497,7 @@ async function initPurchaseManagement() {
     initializeEventListeners();
     loadPurchases();
     loadActiveVendors();
+    loadApprovedRequisitions();
 }
 
 if (document.readyState === 'loading') {
