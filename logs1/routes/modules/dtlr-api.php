250bd8377700aa3da19912c413ecdaea2a9cb5e2
richerain <altamantericjason@gmail.com>
2026-02-15 11:45:20 +0800

    asdasads

--- logs1/routes/modules/dtlr-api.php
+++ logs1/routes/modules/dtlr-api.php
@@ -6,14 +6,14 @@ use Illuminate\Support\Facades\Route;
 // DTLR Module API Routes
 Route::get('/document-tracker', [DTLRController::class, 'getDocumentTracker']);
 Route::post('/document-tracker', [DTLRController::class, 'createDocument']);
+// External dataset with API key (must be before dynamic {docId} routes)
+Route::get('/document-tracker/external', [DTLRController::class, 'externalDocumentTracker']);
 Route::get('/document-tracker/{docId}/view', [DTLRController::class, 'viewDocument']);
 Route::get('/document-tracker/{docId}/download', [DTLRController::class, 'downloadDocument']);
 Route::patch('/document-tracker/{docId}/status', [DTLRController::class, 'updateDocumentStatus']);
-Route::delete('/document-tracker/{docId}', [DTLRController::class, 'deleteDocument']);
+Route::delete('/document-tracker/{docId}', [DTLRController::class, 'deleteDocument'])->where('docId', '^(?!external$).+');
 Route::get('/logistics-record', [DTLRController::class, 'getLogisticsRecord']);
 
-// External dataset with API key
-Route::get('/document-tracker/external', [DTLRController::class, 'externalDocumentTracker']);
 // Additional DTLR routes can be added here as the module develops
 Route::get('/test', function () {
     return response()->json([
